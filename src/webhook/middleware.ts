2298dcae58da889c8f01dc1043c1a7134d5fe5c9
Nicholas Tarsillo <nicholastarsillo@Nicholass-MacBook-Pro.local>
2022-08-20 14:51:55 -0400

    add middleware validation

--- src/webhook/middleware.ts
+++ src/webhook/middleware.ts
@@ -10,6 +10,16 @@ export const makeVerifier =
       const timestamp = req.headers["X-Signature-Timestamp"] as string;
       const body = req.rawBody;
 
+      if (!body) {
+        return res.status(400).send("body is required");
+      }
+      if (!timestamp) {
+        return res.status(400).send("timestamp is required");
+      }
+      if (!signature) {
+        return res.status(400).send("signature is required");
+      }
+
       const isVerified = nacl.sign.detached.verify(
         Buffer.from(timestamp + body),
         Buffer.from(signature, "hex"),
