61608f9dce8ecdfb48eb27321008e59a657252f6
Nicholas Tarsillo <nicholastarsillo@Nicholass-MacBook-Pro.local>
2022-08-20 14:34:26 -0400

    fix sample port

--- sample/register.ts
+++ sample/register.ts
@@ -1,5 +1,5 @@
 import * as config from "./config";
 import * as discord from "../src";
 
-const webhook = discord.webhook({ publicKey: config.publicKey });
+const webhook = discord.webhook({ publicKey: config.publicKey, port: parseInt(process.env.PORT as string) });
 webhook.register(discord.Interaction.Ping, (event) => console.log("got event", event));
